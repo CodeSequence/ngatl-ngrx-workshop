6159a384489a4c1544820dc2e6c0f55e9d9a9a4e
Brandon <robertsbt@gmail.com>
2019-01-09 06:25:24 +0000

    clean up items actions

--- projects/dashboard/src/app/state/items/items.actions.ts
+++ projects/dashboard/src/app/state/items/items.actions.ts
@@ -3,15 +3,15 @@ import { Item } from '../../core/items/item.model';
 
 export enum ItemsActionTypes {
   ItemsAction = '[Items] Action',
-  ItemSelected = '[Items] Selected',
-  LoadItems = '[Items] Load Data',
-  ItemsLoaded = '[Items] Data Loaded',
-  AddItem = '[Items] Add Data',
-  ItemAdded = '[Items] Data Added',
-  UpdateItem = '[Items] Update Data',
-  ItemUpdated = '[Items] Data Updated',
-  DeleteItem = '[Items] Delete Data',
-  ItemDeleted = '[Items] Data Deleted',
+  ItemSelected = '[Items Page] Item Selected',
+  LoadItems = '[Items Page] Load Items',
+  ItemsLoaded = '[Items/API] Items Loaded',
+  AddItem = '[Items Page] Add Item',
+  ItemAdded = '[Items/API] Item Added',
+  UpdateItem = '[Items] Item Updated',
+  ItemUpdated = '[Items/API] Item Updated',
+  DeleteItem = '[Items Page] Delete Item',
+  ItemDeleted = '[Items] Item Deleted',
 }
 
 export class Items implements Action {
@@ -20,46 +20,53 @@ export class Items implements Action {
 
 export class ItemSelected implements Action {
   readonly type = ItemsActionTypes.ItemSelected;
-  constructor(public payload) { }
+
+  constructor(public payload: string) { }
 }
 
 export class LoadItems implements Action {
   readonly type = ItemsActionTypes.LoadItems;
-  constructor() { }
 }
 
 export class ItemsLoaded implements Action {
   readonly type = ItemsActionTypes.ItemsLoaded;
+
   constructor(public payload: Item[]) { }
 }
 
 export class AddItem implements Action {
   readonly type = ItemsActionTypes.AddItem;
+
   constructor(public payload: Item) { }
 }
 
 export class ItemAdded implements Action {
   readonly type = ItemsActionTypes.ItemAdded;
+
   constructor(public payload: Item) { }
 }
 
 export class UpdateItem implements Action {
   readonly type = ItemsActionTypes.UpdateItem;
+
   constructor(public payload: Item) { }
 }
 
 export class ItemUpdated implements Action {
   readonly type = ItemsActionTypes.ItemUpdated;
+
   constructor(public payload: Item) { }
 }
 
 export class DeleteItem implements Action {
   readonly type = ItemsActionTypes.DeleteItem;
+
   constructor(public payload: Item) { }
 }
 
 export class ItemDeleted implements Action {
   readonly type = ItemsActionTypes.ItemDeleted;
+
   constructor(public payload: Item) { }
 }
 
@@ -72,5 +79,4 @@ export type ItemsActions = Items
   | UpdateItem
   | ItemUpdated
   | DeleteItem
-  | ItemDeleted
-;
+  | ItemDeleted;
